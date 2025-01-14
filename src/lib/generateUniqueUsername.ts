6b74b3d3dc05fcb5f186768eeb5d745e2cf81883
Pranav M <pranav.mappoli@walmart.com>
2025-01-14 22:25:16 +0530

    set default username size to 16

--- src/lib/generateUniqueUsername.ts
+++ src/lib/generateUniqueUsername.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 
 function generateUniqueUsername(email:string) {
     const emailPrefix = email.split('@')[0];
-    const userName = `${emailPrefix}_${uuidv4().slice(0, 8)}`;
+    const userName = `${emailPrefix}_${uuidv4()}`.slice(0, 16);
     return userName
 }
 
