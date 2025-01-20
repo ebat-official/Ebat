62e111d95f71025a52fee7efc16aad35818e7324
Pranav M <pranav.mappoli@walmart.com>
2025-01-20 17:44:08 +0530

    fix sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
-
 export default {
   darkMode: ["class"],
   content: [
@@ -51,68 +50,69 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			},
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
+			  sidebar: {
+				DEFAULT: 'hsl(var(--sidebar-background))',
+				foreground: 'hsl(var(--sidebar-foreground))',
+				primary: 'hsl(var(--sidebar-primary))',
+				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+				accent: 'hsl(var(--sidebar-accent))',
+				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+				border: 'hsl(var(--sidebar-border))',
+				ring: 'hsl(var(--sidebar-ring))'
+			}
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		fontFamily: {
-  			inter: [
-  				'var(--font-inter)',
-                    ...defaultTheme.fontFamily.sans
-                ],
-  			'source-serif': [
-  				'var(--font-source-serif)',
-                    ...defaultTheme.fontFamily.serif
-                ],
-  			code: [
-  				'var(--font-code)',
-                    ...defaultTheme.fontFamily.mono
-                ]
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-			  "collapsible-down": {
-				from: { height: "0" },
-				to: { height: "var(--radix-collapsible-content-height)" }
-			  },
-			  "collapsible-up": {
-				from: { height: "var(--radix-collapsible-content-height)" },
-				to: { height: "0" }
-			  }
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-			"collapsible-down": "collapsible-down 0.2s ease-out",
-        	"collapsible-up": "collapsible-up 0.2s ease-out"
-  		}
+		  fontFamily: {
+			inter: [
+				'var(--font-inter)',
+				  ...defaultTheme.fontFamily.sans
+			  ],
+			'source-serif': [
+				'var(--font-source-serif)',
+				  ...defaultTheme.fontFamily.serif
+			  ],
+			code: [
+				'var(--font-code)',
+				  ...defaultTheme.fontFamily.mono
+			  ]
+		},
+		  keyframes: {
+			'accordion-down': {
+				from: {
+					height: '0'
+				},
+				to: {
+					height: 'var(--radix-accordion-content-height)'
+				}
+			},
+			'accordion-up': {
+				from: {
+					height: 'var(--radix-accordion-content-height)'
+				},
+				to: {
+					height: '0'
+				}
+			},
+			"collapsible-down": {
+			  from: { height: "0" },
+			  to: { height: "var(--radix-collapsible-content-height)" }
+			},
+			"collapsible-up": {
+			  from: { height: "var(--radix-collapsible-content-height)" },
+			  to: { height: "0" }
+			}
+		},
+		animation: {
+			'accordion-down': 'accordion-down 0.2s ease-out',
+			'accordion-up': 'accordion-up 0.2s ease-out',
+		  "collapsible-down": "collapsible-down 0.2s ease-out",
+		  "collapsible-up": "collapsible-up 0.2s ease-out"
+		}
+		
   	}
   },
   plugins: [require("tailwindcss-animate")],
