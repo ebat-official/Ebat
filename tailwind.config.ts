90a6b7fbc2469cc351e66dbb5fff2a5437f98839
Pranav M <pranav.mappoli@walmart.com>
2025-01-20 10:43:44 +0530

    add accordian

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,30 +67,6 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-		      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" }
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" }
-        },
-        "collapsible-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-collapsible-content-height)" }
-        },
-        "collapsible-up": {
-          from: { height: "var(--radix-collapsible-content-height)" },
-          to: { height: "0" }
-        }
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "collapsible-down": "collapsible-down 0.2s ease-out",
-        "collapsible-up": "collapsible-up 0.2s ease-out"
-      },
   		fontFamily: {
   			inter: [
   				'var(--font-inter)',
@@ -104,6 +80,38 @@ export default {
   				'var(--font-code)',
                     ...defaultTheme.fontFamily.mono
                 ]
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+			  "collapsible-down": {
+				from: { height: "0" },
+				to: { height: "var(--radix-collapsible-content-height)" }
+			  },
+			  "collapsible-up": {
+				from: { height: "var(--radix-collapsible-content-height)" },
+				to: { height: "0" }
+			  }
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+			"collapsible-down": "collapsible-down 0.2s ease-out",
+        	"collapsible-up": "collapsible-up 0.2s ease-out"
   		}
   	}
   },
