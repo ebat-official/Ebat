eac075869055824e1f0cab399ec1fa471d095e28
Pranav M <pranavmappoli@gmail.com>
2025-03-31 15:39:54 +0530

    fix reset password css

--- src/app/(auth)/resetPassword/page.tsx
+++ src/app/(auth)/resetPassword/page.tsx
@@ -23,7 +23,7 @@ import {
 import { SOMETHING_WENT_WRONG_ERROR } from "@/utils/errors";
 import FormSuccess from "@/components/shared/FormSuccess";
 import FormError from "@/components/shared/FormError";
-interface pageProps {}
+type pageProps = {};
 
 type FormValues = {
 	password: string;
@@ -112,102 +112,98 @@ const ResetPassword: FC<pageProps> = ({}) => {
 
 	return (
 		<div className="m-0 mt-0 font-sans text-base antialiased font-normal transition-all duration-200  ease-soft-in-out text-start leading-default text-slate-500">
-			<section className="max-h-screen">
-				<div className="relative flex items-center p-0 overflow-hidden bg-center bg-cover min-h-[75vh]">
-					<div className="container z-10">
-						<div className="flex flex-wrap mt-0 -mx-3">
-							<div className="flex flex-col w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-6/12 lg:w-5/12 xl:w-4/12">
-								<div className="relative flex flex-col min-w-0 mt-32 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
-									<div className="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
-										<h4 className="relative z-10 font-bold text-center text-transparent md:text-2xl lg:text-3xl md:text-left bg-linear-to-tl from-blue-600 to-cyan-400 bg-clip-text">
-											Reset your password
-										</h4>
-										<p className="mb-0 text-center md:text-left">
-											Enter a new password for your account.
-										</p>
-									</div>
-									<div className="flex-auto p-6">
-										<form role="form" onSubmit={onSubmit} noValidate>
-											<div className="mb-4">
-												<input
-													{...register("password")}
-													type="password"
-													className={cn(
-														"text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-hidden focus:transition-shadow",
-														{
-															"border-red-500": errors?.password,
-														},
-													)}
-													placeholder="Password"
-													aria-label="Password"
-												/>
-												{errors?.password && (
-													<p className="text-sm text-red-500 dark:text-red-900">
-														{errors.password.message}
-													</p>
-												)}
-											</div>
-											<div className="mb-4">
-												<input
-													{...register("confirmPassword")}
-													type="password"
-													className={cn(
-														"text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-hidden focus:transition-shadow",
-														{
-															"border-red-500": errors?.password,
-														},
-													)}
-													placeholder=" Confirm password"
-													aria-label="Password"
-												/>
-												{errors?.confirmPassword && (
-													<p className="text-sm text-red-500 dark:text-red-900">
-														{errors.confirmPassword.message}
-													</p>
-												)}
-											</div>
-											{VerificationStatus.status == SUCCESS && (
-												<>
-													<FormSuccess message="Password reset successful" />
-													<p className="text-sm pt-4 text-slate-400 text-center">
-														You will be redirected in {timer} seconds
-													</p>
-												</>
-											)}
-
-											{VerificationStatus.status === ERROR && (
-												<>
-													<FormError message={VerificationStatus.data} />
-													<p className="text-sm pt-4 text-slate-400 text-center">
-														You will be redirected in {timer} seconds
-													</p>
-												</>
-											)}
-
-											<div className="text-center">
-												<ButtonDark
-													disabled={
-														VerificationStatus.status === SUCCESS ||
-														VerificationStatus.status === ERROR
-													}
-													type="submit"
-													title="Reset Password"
-													loading={VerificationStatus.status === LOADING}
-												/>
-											</div>
-										</form>
-									</div>
-								</div>
+			<section className="relative flex justify-center items-center p-0 overflow-hidden bg-center bg-cover min-h-[75vh] w-full">
+				<div className="container z-10">
+					<div className="flex flex-wrap mt-0 -mx-3 justify-center">
+						<div className="relative flex flex-col min-w-0 mt-32 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
+							<div className="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
+								<h4 className="relative z-10 font-bold text-center text-transparent md:text-2xl lg:text-3xl md:text-left bg-linear-to-tl from-blue-600 to-cyan-400 bg-clip-text">
+									Reset your password
+								</h4>
+								<p className="mb-0 text-center md:text-left">
+									Enter a new password for your account.
+								</p>
 							</div>
-							<div className="w-full max-w-full px-3 lg:grow-0 lg:shrink-0  shrink-0 md:w-6/12">
-								<div className="absolute top-0 hidden w-3/5 h-full -mr-32 overflow-hidden -skew-x-12 -right-40 rounded-bl-xl md:block">
-									<div
-										className="absolute inset-x-0 top-0 z-0 h-full -ml-16 bg-cover "
-										style={{
-											backgroundImage: `url(${backgroundImg.src})`,
-										}}
-									></div>
-								</div>
+							<div className="flex-auto p-6">
+								<form role="form" onSubmit={onSubmit} noValidate>
+									<div className="mb-4">
+										<input
+											{...register("password")}
+											type="password"
+											className={cn(
+												"text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-hidden focus:transition-shadow",
+												{
+													"border-red-500": errors?.password,
+												},
+											)}
+											placeholder="Password"
+											aria-label="Password"
+										/>
+										{errors?.password && (
+											<p className="text-sm text-red-500 dark:text-red-900">
+												{errors.password.message}
+											</p>
+										)}
+									</div>
+									<div className="mb-4">
+										<input
+											{...register("confirmPassword")}
+											type="password"
+											className={cn(
+												"text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-hidden focus:transition-shadow",
+												{
+													"border-red-500": errors?.password,
+												},
+											)}
+											placeholder=" Confirm password"
+											aria-label="Password"
+										/>
+										{errors?.confirmPassword && (
+											<p className="text-sm text-red-500 dark:text-red-900">
+												{errors.confirmPassword.message}
+											</p>
+										)}
+									</div>
+									{VerificationStatus.status == SUCCESS && (
+										<>
+											<FormSuccess message="Password reset successful" />
+											<p className="text-sm pt-4 text-slate-400 text-center">
+												You will be redirected in {timer} seconds
+											</p>
+										</>
+									)}
+
+									{VerificationStatus.status === ERROR && (
+										<>
+											<FormError message={VerificationStatus.data} />
+											<p className="text-sm pt-4 text-slate-400 text-center">
+												You will be redirected in {timer} seconds
+											</p>
+										</>
+									)}
+
+									<div className="text-center">
+										<ButtonDark
+											disabled={
+												VerificationStatus.status === SUCCESS ||
+												VerificationStatus.status === ERROR
+											}
+											type="submit"
+											title="Reset Password"
+											loading={VerificationStatus.status === LOADING}
+										/>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div className="w-full max-w-full px-3 lg:grow-0 lg:shrink-0  shrink-0 md:w-6/12">
+							<div className="absolute top-0 hidden w-3/5 h-full -mr-32 overflow-hidden -skew-x-12 -right-40 rounded-bl-xl md:block">
+								<div
+									className="absolute inset-x-0 top-0 z-0 h-full -ml-16 bg-cover "
+									style={{
+										backgroundImage: `url(${backgroundImg.src})`,
+									}}
+								></div>
 							</div>
 						</div>
 					</div>
