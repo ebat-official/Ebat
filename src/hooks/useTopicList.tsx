12cdcc05645fa644404e2520026de40688604be2
Pranav M <pranavmappoli@gmail.com>
2025-04-12 19:59:01 +0530

    fix

--- src/hooks/useTopicList.tsx
+++ src/hooks/useTopicList.tsx
@@ -3,30 +3,30 @@ import topicsData from "@/utils/subCategoryConfig";
 import { TopicCategory } from "@/utils/types";
 
 const useTopics = (category: TopicCategory) => {
-  if (!category) return { topics: [], searchTopics };
-  // Use the raw data directly from topicsData based on the category
-  const [topics, setTopics] = useState<string[]>(topicsData[category] || []);
+	if (!category) return { topics: [], searchTopics };
+	// Use the raw data directly from topicsData based on the category
+	const [topics, setTopics] = useState<string[]>(topicsData[category] || []);
 
-  useEffect(() => {
-    // Reset to original list when category changes
-    setTopics(topicsData[category] || []);
-  }, [category]);
+	useEffect(() => {
+		// Reset to original list when category changes
+		setTopics(topicsData[category] || []);
+	}, [category]);
 
-  const searchedTopics = useCallback(
-    (query: string): string[] => {
-      if (!topics) return topicsData[category];
-      return topics.filter((topic) =>
-        topic.toLowerCase().includes(query.toLowerCase())
-      );
-    },
-    [topics, category]
-  );
+	const searchedTopics = useCallback(
+		(query: string): string[] => {
+			if (!topics) return topicsData[category];
+			return topics.filter((topic) =>
+				topic.toLowerCase().includes(query.toLowerCase()),
+			);
+		},
+		[topics, category],
+	);
 
-  function searchTopics(query: string) {
-    setTopics(searchedTopics(query));
-  }
+	function searchTopics(query: string) {
+		setTopics(searchedTopics(query));
+	}
 
-  return { topics, searchTopics };
+	return { topics, searchTopics };
 };
 
 export default useTopics;
