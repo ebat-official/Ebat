df2defd1057583fd1b2e02963e16bf3a6f1181f2
Pranav M <pranavmappoli@gmail.com>
2025-04-27 16:37:42 +0530

    fix background shadow

--- src/components/shared/Background.tsx
+++ src/components/shared/Background.tsx
@@ -10,8 +10,8 @@ const Background: FC<BackgroundProps> = ({ shadow = true }) => {
 			<div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
 				{shadow && (
 					<>
-						<div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 dark:opacity-15 blur-[100px]" />
-						<div className="absolute left-0  bottom-1/4 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-10 blur-[100px]" />
+						<div className="fixed left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 dark:opacity-15 blur-[100px]" />
+						<div className="fixed left-0  top-[100px] -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-10 blur-[100px]" />
 					</>
 				)}
 			</div>
