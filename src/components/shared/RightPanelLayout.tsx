264569ff1af926719486dadca1013b6767dc7251
Pranav M <pranavmappoli@gmail.com>
2025-05-11 17:08:11 +0530

    fix layout

--- src/components/shared/RightPanelLayout.tsx
+++ src/components/shared/RightPanelLayout.tsx
@@ -49,23 +49,6 @@ function SidePanel({
 			<input type="checkbox" id="sidepanel-toggle" className="hidden peer" />
 
 			{/* Toggle Button */}
-			<label
-				htmlFor="sidepanel-toggle"
-				className="hidden md:absolute z-50 w-8 h-8 bg-background -translate-x-[50%]  left-0 top-4 rounded-md shadow-md cursor-pointer md:flex justify-center items-center"
-			>
-				<ChevronLeft
-					className={cn(
-						"h-4 w-4 transition-transform ease-in-out duration-700 ",
-						"peer-checked:rotate-180",
-					)}
-				/>
-				<ChevronLeft
-					className={cn(
-						"h-4 w-4 transition-transform ease-in-out duration-700 rotate-180",
-						"peer-checked:rotate-180",
-					)}
-				/>
-			</label>
 
 			{/* Side Panel */}
 			<div
@@ -76,6 +59,23 @@ function SidePanel({
 				)}
 			>
 				{children}
+				<label
+					htmlFor="sidepanel-toggle"
+					className="hidden md:absolute z-50 w-8 h-8 bg-background -translate-x-[50%]  left-0 top-4 rounded-md shadow-md cursor-pointer md:flex justify-center items-center"
+				>
+					<ChevronLeft
+						className={cn(
+							"h-4 w-4 transition-transform ease-in-out duration-700 ",
+							"peer-checked:rotate-180",
+						)}
+					/>
+					<ChevronLeft
+						className={cn(
+							"h-4 w-4 transition-transform ease-in-out duration-700 rotate-180",
+							"peer-checked:rotate-180",
+						)}
+					/>
+				</label>
 			</div>
 		</div>
 	);
