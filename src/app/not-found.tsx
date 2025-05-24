8fccbf66fcc997679248ec00271d253c8f7d591c
Pranav M <pranavmappoli@gmail.com>
2025-05-24 23:12:21 +0530

    bats images

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import icon from "@/assets/svg/oops404.svg";
+import icon from "@/assets/img/404.webp";
 import Link from "next/link";
 import ButtonBlue from "@/components/shared/ButtonBlue";
 
@@ -7,17 +7,17 @@ export default function Component() {
 	return (
 		<div className="flex min-h-[90dvh] flex-col items-center justify-center  px-4 py-12 ">
 			<div className="container mx-auto flex max-w-4xl flex-col-reverse items-center gap-2 md:flex-row md:gap-12">
-				<div className="flex flex-1 flex-col items-center justify-center space-y-4 text-center md:items-start md:text-left">
+				<div className="flex flex-1 flex-col items-center justify-center space-y-4 text-center  md:text-left gap-4">
 					<h1 className="text-2xl font-bold tracking-tighter sm:text-5xl">
 						Hey Coder! 👨‍💻
 					</h1>
-					<p className="max-w-[450px] text-gray-500 dark:text-gray-400">
-						Our robot guide got a bit too curious and broke down. You seem to
-						have landed on a page that doesn’t exist!
+					<p className="max-w-[450px] text-gray-500 dark:text-gray-400 text-center">
+						Our adventurous bat tried to fly here, but even it couldn't reach
+						this page.
 					</p>
 
 					<Link href="/" prefetch={false}>
-						<ButtonBlue type="submit" title="Go to Homepage" className="w-30" />
+						<ButtonBlue type="submit" title="Go to Homepage" />
 					</Link>
 				</div>
 				<div className="flex flex-1 justify-center">
@@ -26,7 +26,7 @@ export default function Component() {
 						width={500}
 						height={500}
 						alt="404 Illustration"
-						className="max-w-[300px] sm:max-w-[400px] md:max-w-[500px]"
+						className="max-w-[300px] sm:max-w-[400px] md:max-w-[400px]"
 						style={{ aspectRatio: "500/500", objectFit: "cover" }}
 					/>
 				</div>
