551210ab482ea8a3a94d381bfbc15081cff5dc90
Pranav M <Pranav.Mappoli@walmart.com>
2025-06-22 12:20:54 +0530

    chlng create mode

---
+++ src/app/[category]/[subCategory]/challenges/create/[[...postId]]/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import { useParams, notFound } from "next/navigation";
+import isValidCategory from "@/utils/isValidCategory";
+import isValidSubCategory from "@/utils/isValidSubCategory";
+import { POST_ACTIONS } from "@/utils/contants";
+import PostCreateEdit from "@/components/post edit/PostCreateEdit";
+import { PostType } from "@prisma/client";
+
+function Page() {
+	const {
+		category: categoryRoute,
+		subCategory: subCategoryRoute,
+		postId: postIdParam,
+	} = useParams();
+
+	const subCategory = (
+		Array.isArray(subCategoryRoute) ? subCategoryRoute[0] : subCategoryRoute
+	)?.toUpperCase();
+	const category = (
+		Array.isArray(categoryRoute) ? categoryRoute[0] : categoryRoute
+	)?.toUpperCase();
+
+	const postId = Array.isArray(postIdParam) ? postIdParam[0] : postIdParam;
+
+	// Validate dynamic parameters
+	if (
+		!category ||
+		!subCategory ||
+		!isValidSubCategory(subCategory) ||
+		!isValidCategory(category)
+	) {
+		notFound();
+	}
+
+	return (
+		<PostCreateEdit
+			category={category}
+			subCategory={subCategory}
+			postId={postId}
+			action={POST_ACTIONS.CREATE}
+			postType={PostType.QUESTION}
+		/>
+	);
+}
+
+export default Page;
