8d293c2a650f260b0aa697a892a3f944ccd51c4f
Pranav M <pranavmappoli@gmail.com>
2025-07-05 00:08:36 +0530

    fix front page

--- src/components/feed/AddPostRoundButton.tsx
+++ src/components/feed/AddPostRoundButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IoMdAdd } from "react-icons/io";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useParams } from "next/navigation";
 import {
 	Tooltip,
 	TooltipContent,
@@ -10,11 +10,21 @@ import {
 
 const AddPostRoundButton: React.FC = () => {
 	const pathname = usePathname();
+	const params = useParams();
+
+	// Get subcategory from params, default to 'blogs' if not present
+	const subCategory = params.subCategory || "blogs";
+
+	// If we have a subcategory in the URL, use the current path + /create
+	// If we don't have a subcategory, append the default subcategory + /create
+	const createPath = params.subCategory
+		? `${pathname}/create`
+		: `${pathname}/${subCategory}/create`;
 
 	return (
 		<Tooltip>
 			<TooltipTrigger asChild>
-				<Link href={`${pathname}/create`}>
+				<Link href={createPath}>
 					<button
 						type="button"
 						className="h-10 w-10 rounded-full flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 shadow-md hover:scale-105 transition-transform cursor-pointer"
