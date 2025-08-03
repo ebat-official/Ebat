892da0c876262a0f4465929d1cd074a9aef23913
Pranav M <pranavmappoli@gmail.com>
2025-08-04 01:54:31 +0530

    fix security

--- next.config.ts
+++ next.config.ts
@@ -34,16 +34,10 @@ const nextConfig: NextConfig = {
 						key: "Cross-Origin-Opener-Policy",
 						value: "same-origin",
 					},
-
-					// {
-					// 	key: "Content-Security-Policy",
-					// 	value:
-					// 		"default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:;",
-					// },
-					// {
-					// 	key: "X-Content-Type-Options",
-					// 	value: "nosniff",
-					// },
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
 					{
 						key: "X-Frame-Options",
 						value: "DENY",
