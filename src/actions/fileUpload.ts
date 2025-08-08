c9a652d1650f19045b0bdd3d04f76a8ff1a84187
Pranav M <pranavmappoli@gmail.com>
2025-08-09 00:51:35 +0530

    fix image upload

--- src/actions/fileUpload.ts
+++ src/actions/fileUpload.ts
@@ -27,7 +27,7 @@ const s3 = new S3Client({
 	endpoint: process.env.AWS_ENDPOINT!,
 	credentials: {
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID!,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
 	},
 });
 
