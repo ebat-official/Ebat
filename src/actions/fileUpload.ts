1b579684403230eb47dc6b14ab7bd6cb0ef9a965
Pranav M <pranavmappoli@gmail.com>
2025-08-08 23:01:11 +0530

    fix env key

--- src/actions/fileUpload.ts
+++ src/actions/fileUpload.ts
@@ -26,8 +26,8 @@ const s3 = new S3Client({
 	region: process.env.AWS_BUCKET_REGION!,
 	endpoint: process.env.AWS_ENDPOINT!,
 	credentials: {
-		accessKeyId: process.env.AWS_ACCESS_KEY!,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID!,
 	},
 });
 
